ac613cdf793e24885a67cc51924735e3fc41ec7e
gonzariquelme6 <gonzariquelme66@gmail.com>
2022-04-11 19:36:02 -0300

    Creo migraciones adicionales

---
+++ database/migrations/2022_04_07_215441_create_driver_in_team_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use App\Models\Team;
+use App\Models\Driver;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('driver_in_teams', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(Team::class)->constrained();
+            $table->unsignedBigInteger('driver_1_id');
+            $table->foreign('driver_1_id')->references('id')->on('drivers');
+            $table->unsignedBigInteger('driver_2_id');
+            $table->foreign('driver_2_id')->references('id')->on('drivers');
+            //$table->foreignIdFor(Driver::class)->constrained();
+            //$table->foreignIdFor(Driver::class)->constrained();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('driver_in_teams');
+    }
+};
